871586a875787efd25433d2fc47e7f51c9feb0c3
Rodrigues <Gabrielle.Rodrigues@dieboldnixdorf.com>
2020-02-28 17:51:04 -0300

    Introdução ao Javascript

---
+++ main.js
@@ -0,0 +1,81 @@
+/*
+var nome = "Gabrielle Rodrigues";
+var idade = 21;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+// alert(nome + " tem " + idade + " anos");
+// alert(idade + idade2);
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.toUpperCase());
+*/
+
+
+// var lista = ["maça", "pêra", "laranja"];
+// lista.push("uva"); //adiciona um elemento no final da lista
+// lista.pop(); remove o último elemento
+// console.log(lista.length);
+// console.log(lista.reverse());
+// console.log(lista.toString());
+// console.log(lista.join(" | "));
+
+// var fruta = {nome:"maça", cor:"vermelha"}
+// console.log(fruta.cor);
+
+
+// var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+// console.log(frutas[1].nome);
+
+
+// var idade = prompt("Qual sua idade?");
+
+// if (idade >= 18) {
+//     alert("maior de idade");
+// }else {
+//     alert("menor de idade");
+// };
+
+// var count = 0;
+// while(count <= 5){
+//     console.log(count);
+//     count = count + 1;
+//     count++;
+//     alert(count);
+// }
+
+// var count = 0;
+// for (count = 0; count <=5; count++){
+//     alert(count);
+// };
+
+// var d = new Date();
+// alert(d.getDay()+1);
+// alert(d.getMonth()+1);
+// alert(d.getMinutes());
+// alert(d.getHours());
+
+
+// function soma(n1,n2) {
+//     return n1 + n2;
+// }
+
+// function setReplace(frase, nome, novoNome){
+//     return frase.replace(nome,novoNome)
+// }
+
+// alert(soma(5,10));
+// alert(setReplace("Vai Japão", "Japão", "Brasil"));
+
+// function validaIdade(idade) {
+//     var validar;
+//     if (idade >= 18) {
+//        validar = true
+//     }else{
+//         validar = false
+//     }
+//     return validar;
+// }
+
+// var idade = prompt("Qual a sua idade ?")
+// console.log(validaIdade(idade));
